c7b2fa9b8e7ac02c83c91bb83f3cb866be2b2551
ShubhamRoy96 <36968449+ShubhamRoy96@users.noreply.github.com>
2018-03-02 23:38:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,13 +13,15 @@ app.get('/article-one',function (req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 
-app.get('/article-two',function (req,res) {
-    res.send("Article two");
+app.get('/article-one',function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
 });
 
-app.get('/article-three',function (req,res) {
-    res.send("Article three");
+app.get('/article-one',function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
 });
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
